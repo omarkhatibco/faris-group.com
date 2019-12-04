ac3c0e9cf98d6a6d013080152b21c91030f4346d
Fouad Khatib <f.khatib@gingco.net>
2019-12-04 11:29:19 +0100

    svg optimization

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -57,7 +57,7 @@ const AboutUs: AppPage = () => {
 					</Box>
 					<List d='flex' width={6 / 12} padding={8}>
 						<ListIcon icon={FaBuilding} aria-label='Building' size='4rem' color='green.500' />
-						<BuildingSVG></BuildingSVG>
+						<BuildingSVG /> 
 						<ListItem>
 							Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet
 						</ListItem>
