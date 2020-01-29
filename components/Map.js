158106721dbcd9a49ec401df7ac69ee8d700be18
Omar Khatib <omar@omarkhatib.co>
2020-01-29 21:54:12 +0100

    Fix bug with map

--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,10 @@
 import { AspectRatioBox, Box } from '@chakra-ui/core';
 import useMedia from 'react-use/lib/useMedia';
 export const Map = () => {
-	const isLg = useMedia('(min-width: 768px)');
+	const isMd = useMedia('(max-width: 768px)');
 
 	return (
-		<AspectRatioBox ratio={isLg ? 4 / 1 : 1}>
+		<AspectRatioBox ratio={isMd ? 1 : 4 / 1}>
 			<Box
 				as='iframe'
 				src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6021.243452206435!2d28.941553000000003!3d41.011652!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb571cad62b830605!2z2YXYrNmF2YjYudipINin2YTZgdin2LHYsyDZhNmE2KfYs9iq2LTYp9ix2KfYqiDYp9mE2YLYp9mG2YjZhtmK2Kk!5e0!3m2!1sar!2str!4v1575227398841!5m2!1sar!2str'></Box>
