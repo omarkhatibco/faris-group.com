d729682c457a21faacedd9bec7f813bd1ba551b9
Omar Khatib <omar@omarkhatib.co>
2020-02-11 21:46:01 +0100

    breadcurmb

---
+++ components/PropertyFacts.js
@@ -0,0 +1,22 @@
+import { Box, Flex } from '@chakra-ui/core';
+import { ApartmentInfo } from '~components';
+import { formatNumber } from '~utls';
+
+export const PropertyFacts = ({ data }) => {
+	const { appartment_count, lot_size } = data;
+	return (
+		<Flex mx={-4}>
+			<Box px={4}>
+				<ApartmentInfo
+					title='مساحة المشروع'
+					icon='bed'
+					value={formatNumber(lot_size)}
+					suffix='متر<sup>٢</sup>'
+				/>
+			</Box>
+			<Box px={4}>
+				<ApartmentInfo title='عدد الشقق' icon='bed' value={formatNumber(appartment_count)} />
+			</Box>
+		</Flex>
+	);
+};
--- components/PropertyHeading.tsx
+++ components/PropertyHeading.tsx
@@ -1,15 +1,43 @@
-import { Spinner, Flex, Box, Grid, Image, IconButton, Heading } from '@chakra-ui/core';
+import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Heading, Icon } from '@chakra-ui/core';
+import Link from 'next/link';
+import { useContext } from 'react';
+import { ConfigContext, PropertyFacts } from '~components';
+
 export const PropertyHeading = ({ data }) => {
+	const { locations } = useContext<any>(ConfigContext);
+	const { location, sublocation } = data;
+	const locationObj = locations?.find(({ slug }) => slug === location);
+	const sublocationObj = locationObj?.fgw_sublocations?.find(({ slug }) => slug === sublocation);
+
 	return (
 		<Box>
-			<Heading
-				as='h1'
-				mb='2'
-				color='green.500'
-				fontSize={['2xl', '5xl']}
-				textTransform='uppercase'
-				fontFamily='inherit'
-				dangerouslySetInnerHTML={{ __html: data?.title?.rendered }}></Heading>
+			<Box mb={2}>
+				<Heading
+					as='h1'
+					mb='2'
+					color='green.500'
+					fontSize={['2xl', '5xl']}
+					textTransform='uppercase'
+					fontFamily='inherit'
+					dangerouslySetInnerHTML={{ __html: data?.title?.rendered }}></Heading>
+			</Box>
+			<Breadcrumb mb={6} spacing='8px' separator={<Icon color='gray.300' name='chevron-left' />}>
+				<BreadcrumbItem>
+					<Link href='/'>
+						<BreadcrumbLink>{locationObj?.title}</BreadcrumbLink>
+					</Link>
+				</BreadcrumbItem>
+				<BreadcrumbItem>
+					<Link href='/'>
+						<BreadcrumbLink>{sublocationObj?.title}</BreadcrumbLink>
+					</Link>
+				</BreadcrumbItem>
+
+				<BreadcrumbItem isCurrentPage>
+					<BreadcrumbLink>{data?.title?.rendered}</BreadcrumbLink>
+				</BreadcrumbItem>
+			</Breadcrumb>
+			<PropertyFacts data={data} />
 			<Box as='hr' my={8}></Box>
 		</Box>
 	);
