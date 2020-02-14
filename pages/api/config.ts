6a074aec42d935fad85f4365bf44b3489d466f16
Omar Khatib <omar@omarkhatib.co>
2020-02-14 16:05:25 +0100

    ..

--- pages/api/config.ts
+++ pages/api/config.ts
@@ -6,6 +6,6 @@ export default async (req, res) => {
 
 		res.status(200).json(config.json());
 	} catch (error) {
-		res.status(500).json({ message: 'internal server error' });
+		res.status(500).json({ message: 'internal server error', error });
 	}
 };
