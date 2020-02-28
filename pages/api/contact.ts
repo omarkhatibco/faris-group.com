9ef1ed8eff5f5508cda876fee77d03936248c8fb
Omar Khatib <omar@omarkhatib.co>
2020-02-28 03:08:43 +0100

    .....

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,22 +1,18 @@
 export default async (req, res) => {
-	try {
-		if (req.method === 'POST') {
-			const { body } = req;
-			const contact = await fetch(`${process.env.BACKEND_URL}/api/contact`, {
-				method: 'post',
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-				},
-				body: new URLSearchParams(body),
-			}).catch(e => console.log(e));
-			const contactJson = await contact.json();
-			res.status(contact.status).json(contactJson);
-		} else {
-			res.status(405).json({
-				message: 'not allowed',
-			});
-		}
-	} catch (error) {
-		res.status(500).json({ message: 'internal server error', error });
+	if (req.method === 'POST') {
+		const { body } = req;
+		const contact = await fetch(`${process.env.BACKEND_URL}/api/contact`, {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: new URLSearchParams(body),
+		});
+		const contactJson = await contact.json();
+		res.status(contact.status).json(contactJson);
+	} else {
+		res.status(405).json({
+			message: 'not allowed',
+		});
 	}
 };
